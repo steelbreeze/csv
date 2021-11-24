c59c64304b84e2a26843b2e8b0594933fce143a0
David Mesquita-Morris <mesmo@steelbreeze.net>
2021-11-24 07:32:00 +0000

    Alpha 3

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,5 +1,6 @@
 import * as csv from '..';
 
-const data = '\uFEFFa,b,c\r"1",2,3\n4,"5",6\r\n7,"The number eight: ""8""",9\r"a\r\na",b,"c, d"\n12';
+const data = 'a,b,c\r"1",2,3\n4,"5",6\r\n7,"The number eight: ""8""",9\r"a\r\na",b,"c, d"\n12';
 
 console.log(csv.parse(data));
+
